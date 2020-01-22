efff2fcb3c691367fa610548917621e5c045638f
Francisco Bento <franciscobdsjunior@gmail.com>
2020-01-22 11:58:19 -0300

    :recycle: Refactor QueryCollector to add exec methods

--- promsql/sql_query.go
+++ promsql/sql_query.go
@@ -23,6 +23,8 @@ func NewSQLQuery(namedQuery *promquery.NamedQueryCollector, db DBQueryProxy) *Pr
 type DBQueryProxy interface {
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 }
 
 func (srv *PromSQLQuery) Query(query string, args ...interface{}) (*sql.Rows, error) {
@@ -52,6 +54,52 @@ func (srv *PromSQLQuery) QueryContext(ctx context.Context, query string, args ..
 		srv.namedQuery.TotalFailures.Inc()
 	} else {
 		srv.namedQuery.TotalSuccess.Inc()
+	}
+
+	return res, err
+}
+
+func (srv *PromSQLQuery) Exec(Exec string, args ...interface{}) (sql.Result, error) {
+	srv.namedQuery.TotalCalls.Inc()
+
+	start := time.Now()
+	res, err := srv.db.Exec(Exec, args...)
+	srv.namedQuery.TotalDuration.Add(time.Since(start).Seconds())
+
+	if err != nil {
+		srv.namedQuery.TotalFailures.Inc()
+	} else {
+		rowsAffected, rowErr := res.RowsAffected()
+
+		if rowErr != nil {
+			srv.namedQuery.TotalFailures.Inc()
+		} else {
+			srv.namedQuery.TotalSuccess.Inc()
+			srv.namedQuery.TotalRowsAffected.Add(float64(rowsAffected))
+		}
+	}
+
+	return res, err
+}
+
+func (srv *PromSQLQuery) ExecContext(ctx context.Context, Exec string, args ...interface{}) (sql.Result, error) {
+	srv.namedQuery.TotalCalls.Inc()
+
+	start := time.Now()
+	res, err := srv.db.ExecContext(ctx, Exec, args...)
+	srv.namedQuery.TotalDuration.Add(time.Since(start).Seconds())
+
+	if err != nil {
+		srv.namedQuery.TotalFailures.Inc()
+	} else {
+		rowsAffected, rowErr := res.RowsAffected()
+
+		if rowErr != nil {
+			srv.namedQuery.TotalFailures.Inc()
+		} else {
+			srv.namedQuery.TotalSuccess.Inc()
+			srv.namedQuery.TotalRowsAffected.Add(float64(rowsAffected))
+		}
 	}
 
 	return res, err
