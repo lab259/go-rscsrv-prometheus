9ea34ea451c69b925d858506901f58db9a39c7b0
Francisco Bento <franciscobdsjunior@gmail.com>
2020-01-23 09:46:14 -0300

    :recycle: Refactor QueryCollector metrics prefix

--- promquery/query_collector.go
+++ promquery/query_collector.go
@@ -21,7 +21,7 @@ type QueryCollectorOpts struct {
 	Prefix string
 }
 
-var queryCollectorLabels = []string{"queryName"}
+var queryCollectorLabels = []string{"name"}
 
 // NewQueryCollector returns a new QueryCollector pointer
 func NewQueryCollector(opts *QueryCollectorOpts) *QueryCollector {
@@ -34,28 +34,28 @@ func NewQueryCollector(opts *QueryCollectorOpts) *QueryCollector {
 	return &QueryCollector{
 		totalCalls: prometheus.NewCounterVec(
 			prometheus.CounterOpts{
-				Name: fmt.Sprintf("db_%stotal_calls", prefix),
+				Name: fmt.Sprintf("namedqry_%stotal_calls", prefix),
 				Help: "The total number of calls from a query",
 			},
 			queryCollectorLabels,
 		),
 		totalDuration: prometheus.NewCounterVec(
 			prometheus.CounterOpts{
-				Name: fmt.Sprintf("db_%stotal_duration", prefix),
+				Name: fmt.Sprintf("namedqry_%stotal_duration", prefix),
 				Help: "The total duration (in seconds) from a query processed",
 			},
 			queryCollectorLabels,
 		),
 		totalSuccess: prometheus.NewCounterVec(
 			prometheus.CounterOpts{
-				Name: fmt.Sprintf("db_%stotal_success", prefix),
+				Name: fmt.Sprintf("namedqry_%stotal_success", prefix),
 				Help: "The total number of a query processed with success",
 			},
 			queryCollectorLabels,
 		),
 		totalFailures: prometheus.NewCounterVec(
 			prometheus.CounterOpts{
-				Name: fmt.Sprintf("db_%stotal_failures", prefix),
+				Name: fmt.Sprintf("namedqry_%stotal_failures", prefix),
 				Help: "The total number of a query processed with failure",
 			},
 			queryCollectorLabels,
